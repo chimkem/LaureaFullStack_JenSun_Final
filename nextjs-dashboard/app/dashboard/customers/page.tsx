3dc1d484ff30ef2d55b882839e2987b21d9cfee0
I'm Just Ken <126956034+chimkem@users.noreply.github.com>
2025-05-15 22:00:33 +0300

    lisää suomennoksia ja koristelua

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,9 +1,17 @@
 import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
 export default function Page() {
-  return <p>Ei asiakkaita...</p>;
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Asiakkaat 🌿
+      </h1>
+      <p>Ei asiakkaita...</p>
+    </div>
+    );
 }
